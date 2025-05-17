41457146413cd510566e20436a6eb397d59873d2
KevenRodrigues6868 <kevenlucas2015@gmail.com>
2025-05-17 15:16:09 -0300

    first commit

---
+++ TecGamer/src/main/java/com/tec_gamer/TecGamer/TecGamerApplication.java
@@ -0,0 +1,13 @@
+package com.tec_gamer.TecGamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecGamerApplication.class, args);
+	}
+
+}
---
+++ TecGamer/src/test/java/com/tec_gamer/TecGamer/TecGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tec_gamer.TecGamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
